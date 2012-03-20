a3335980f3e02468d903398a0e7623466a669e33
Stefan Kalevski <kalevskis@gmail.com>
2012-03-20 11:30:14 +0100

    1

---
+++ account_reactivate.php
@@ -0,0 +1,32 @@
+<?php
+
+include_once('./config/config.php');												                                       // Include Configuration
+include_once(ROOT_DIR_PATH.'connection/dbConnection.php');				                   				   // Include Database Connection
+include_once(ROOT_DIR_PATH.'classes/common/template_reader.class.php');		          				 // Include Templates Reading Class
+include_once(ROOT_DIR_PATH.'classes/user/user_authentication.class.php');               // Include UserAuthentication Class
+
+$btfb_obj_authentication      = new UserAuthentication();
+$btfb_obj_template            = new TemplateReader;		
+	
+$btfb_array_value             = array_merge($btfb_array_value,$btfb_site_language_label);
+if(isset($_POST['account']))
+{
+ 	$btfb_response              = $btfb_obj_authentication -> sendAccountReactivationRequestForExcessLimit();
+ 	$btfb_array_value['center'] = $btfb_response; 		
+}
+else 
+{
+  if($_SESSION['System_Language']=="serbian")
+  $btfb_array_value['center'] = $btfb_obj_template->showRegForm(ROOT_LANG_TEMPLATE_PATH."serbian/account_reactivation_for_exceed_limit.html",$btfb_array_value);		
+  else 
+  $btfb_array_value['center'] = $btfb_obj_template->showRegForm(ROOT_LANG_TEMPLATE_PATH."english/account_reactivation_for_exceed_limit.html",$btfb_array_value);		
+}
+
+
+$btfb_array_value['top']      = $btfb_obj_template->showRegForm(ROOT_TEMPLATE_PATH."user/index_topnotlogged.html",$btfb_array_value);		
+$btfb_array_value['bottom']   = $btfb_obj_template->showRegForm(ROOT_TEMPLATE_PATH."user/index_bottomnotlogged.html",$btfb_array_value);		
+
+
+echo $btfb_obj_template->showRegForm(ROOT_TEMPLATE_PATH."user/user_template_nologin.html",$btfb_array_value);
+
+?>
